0bd4a25b2d70668a4feff3600d1bc1e35ba67f27
leejk118 <leejk118@gmail.com>
2020-05-05 08:56:09 +0900

    AWS setup

--- src/main/java/com/jklee/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jklee/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.jklee.springboot.config.auth.dto;
 import com.jklee.springboot.web.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
