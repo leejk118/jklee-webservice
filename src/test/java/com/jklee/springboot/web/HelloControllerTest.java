f3ddf5b989cc9c1ce0767cfe08b39104383f3e51
leejk118 <leejk118@gmail.com>
2020-05-05 00:10:34 +0900

    modify

--- src/test/java/com/jklee/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jklee/springboot/web/HelloControllerTest.java
@@ -1,11 +1,15 @@
 package com.jklee.springboot.web;
 
+import com.jklee.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
 
 
 import static org.hamcrest.Matchers.is;
@@ -14,13 +18,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloReturn() throws Exception {
         String hello = "Hello";
 
@@ -30,6 +37,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDtoReturn() throws Exception {
         String name = "Hello";
         int amount = 1000;
