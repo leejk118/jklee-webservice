a7450b75787e4fe1559ed7f5c720d3f4c3a73e27
leejk118 <leejk118@gmail.com>
2020-05-11 14:36:54 +0900

    modify profile.sh 오타수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,13 +5,13 @@ function find_idle_profile()
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ]
-    then CURRENT_PROFILE=reals
+    then CURRENT_PROFILE=real2
     else CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
     then IDLE_PROFILE=real2
-    else IDEL_PROFILE=real1
+    else IDLE_PROFILE=real1
     fi
 
     echo "${IDLE_PROFILE}"
@@ -21,7 +21,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real1} ]
+    if [ ${IDLE_PROFILE} == real1 ]
     then echo "8081"
     else echo "8082"
     fi
